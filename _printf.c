2f3171c1265de857d6f49ac84ca575918534b48a
SimzMapisa <simbarashe795@gmail.com>
2023-07-27 23:40:43 +0200

    test sln

--- _printf.c
+++ _printf.c
@@ -1,73 +1,52 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
 
-void print_buffer(char buffer[], int *buff_ind);
+int _printf(const char *format, ...);
 
-/**
- * _printf - printf func
- * @format: format
- *
- * Return: Printed chars
- */
+int _printf(const char *format, ...) {
 
-int _printf(const char *format, ...)
-{
-	int i;
-	int printed = 0;
-	int printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
+    int printed_chars = 0;
+    va_list args;
+    va_start(args, format);
 
-	if (format == NULL)
-	{
-		return (-1);
-	}
+    while (*format) {
+        if (*format == '%') {
+            format++; /* Move past the '%' */
+            switch (*format) {
+                case 'c': {
+                    int c = va_arg(args, int);
+                    putchar(c);
+                    printed_chars++;
+                    break;
+                }
+                case 's': {
+                    const char *str = va_arg(args, const char *);
+                    while (*str) {
+                        putchar(*str);
+                        str++;
+                        printed_chars++;
+                    }
+                    break;
+                }
+                case '%': {
+                    putchar('%');
+                    printed_chars++;
+                    break;
+                }
+                default:
+                    /* Unsupported conversion specifier, just print the '%' */
+                    putchar('%');
+                    printed_chars++;
+                    break;
+            }
+        } else {
+            putchar(*format);
+            printed_chars++;
+        }
+        format++;
+    }
 
-	va_start(list, format);
-
-	for (i = 0; format && format[i] != '\0'; i++)
-	{
-		if (format[i] != "%")
-		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-			{
-				print_biffer(buffer, &buffer_ind);
-				printed_chars++;
-			}
-		}
-		else
-		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list,
-					buffer, flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
-		}
-	}
-
-	print_buffer(buffer, &buff_ind);
-	va_end(list);
-	return (printed_chars);
+    va_end(args);
+    return printed_chars;
 }
 
-/**
- * print_buffer - Prints the contents of the buffer
- * @buffer: array of chars
- * @buff_ind: index
- */
-
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (buff_ind > 0)
-	{
-		write(1, &buffer[0], *buff_ind);
-	}
-	*buff_ind = 0;
-}
